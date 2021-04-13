9c4432dd587a6bd304fe493b2532963d93208112
zipang <christophe.Desguez@gmail.com>
2021-04-13 22:42:46 +0200

    Manage to pass the global styles values to the theme

---
+++ src/styles/x-track-theme.js
@@ -0,0 +1,35 @@
+export const fonts = {
+	headings: `'Conduit ITC W02 Bold', sans`,
+	body: `Raleway`
+};
+
+export const breakpoints = {
+	sm: "480px",
+	md: "768px",
+	lg: "1080px",
+	xl: "1400px"
+};
+
+export const colors = {
+	brand: {
+		black: "#000",
+		blue: "#00F",
+		yellow: "#FF0",
+		white: "#FFF"
+	}
+};
+
+export const global = {
+	html: {
+		fontSize: {
+			sm: "18px",
+			md: "18px",
+			lg: "20px",
+			xl: "22px"
+		}
+	},
+	body: {
+		minWidth: "480px",
+		overflowX: "hidden"
+	}
+};

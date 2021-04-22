09db0a5e35f461d8369d6ab91b185e86abe0ae24
zipang <christophe.desguez@gmail.com>
2021-04-22 09:40:06 +0200

    Base components

---
+++ src/components/base/Section.js
@@ -0,0 +1,7 @@
+import { Stack } from "@chakra-ui/react";
+
+const Section = (props) => (
+	<Stack spacing="1.5rem" width="100%" pt="1rem" pb="1rem" {...props} />
+);
+
+export default Section;
---
+++ src/components/base/index.js
@@ -0,0 +1,5 @@
+export { Link, TextLink } from "./Link.js";
+export { default as Main } from "./Main.js";
+export { default as Section } from "./Section.js";
+export { default as Container } from "./Container.js";
+export { default as HeroSection } from "./HeroSection.js";

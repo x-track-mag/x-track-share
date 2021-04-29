e352cd4bef6412bd7741e4d4f09e598c0186ab14
zipang <christophe.Desguez@gmail.com>
2021-04-29 19:07:27 +0200

    Use AspectRatio to have a 4/3  responsive rectangle

--- src/components/base/Folder.js
+++ src/components/base/Folder.js
@@ -1,14 +1,16 @@
-import { Link } from "@chakra-ui/layout";
-import { Heading } from "@chakra-ui/layout";
-import { Box } from "@chakra-ui/layout";
+import { Link, AspectRatio, Box, Heading } from "@chakra-ui/layout";
 
 const Folder = ({ title, path }) => (
 	<Link as={NextLink} ref={path}>
-		<Box color="brand.blue" _hover={}>
+		<AspectRatio
+			maxW={{ sm: "90vw", lg: "30vw", xl: "400rem" }}
+			color="brand.blue"
+			_hover="brand.yellow"
+		>
 			<Heading as="h3" color="brand.yellow">
 				{title}
 			</Heading>
-		</Box>
+		</AspectRatio>
 	</Link>
 );
 

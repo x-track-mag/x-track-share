7c38f7ee130694d659287a37d17eb68da01fc12a
zipang <christophe.Desguez@gmail.com>
2021-04-29 22:11:20 +0200

    We must not directly invoke the Cloudinary API from the front. Use a Proxy to hide the credentials

---
+++ src/pages/api/share/[uid].js
@@ -0,0 +1,17 @@
+import CloudinaryClient from "@lib/services/CloudinaryClient.js";
+
+/**
+ * The API entry point (GET) to get the content of a shared folder
+ */
+export default async (req, resp) => {
+	const { uid } = req.query; // UID of the shared folder
+	try {
+		const respBody = await CloudinaryClient.getContent(`share/${uid}`);
+		resp.json(respBody);
+	} catch (err) {
+		resp.status(err.code || 500).json({
+			success: false,
+			error: err.message
+		});
+	}
+};

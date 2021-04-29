cda0f10462bd3b807f20cf83788e26cf08eba127
zipang <christophe.Desguez@gmail.com>
2021-04-29 19:06:25 +0200

    Useful Array utils

---
+++ src/lib/utils/Arrays.js
@@ -0,0 +1,51 @@
+// It's usually a bid idea to extend  prototypes but..
+// it can be damn useful too to chain array manipulations
+
+const ArrayExtensions = {
+	/**
+	 * Just scramble elements in place and return the array
+	 * @return {Array}
+	 */
+	shuffle: function () {
+		let len = this.length,
+			i;
+		while (len) {
+			i = (Math.random() * len--) >>> 0;
+			[this[len], this[i]] = [this[i], this[len]];
+		}
+		return this;
+	},
+	/**
+	 * Move an element from one position to another
+	 * @param {Number} from
+	 * @param {Number} to
+	 * @return {Array}
+	 */
+	move: function (from, to) {
+		const arr = this,
+			len = arr.length;
+
+		if (!arr[from]) {
+			// index not existing
+			return;
+		}
+
+		const startIndex = from < 0 ? len + from : from;
+
+		if (startIndex >= 0 && startIndex < len) {
+			const endIndex = to < 0 ? len + to : to;
+
+			const [item] = arr.splice(from, 1);
+			arr.splice(endIndex, 0, item);
+		}
+		return arr;
+	}
+};
+
+export const extendsPrototype = (proto) => {
+	if (!proto.move) proto.move = ArrayExtensions.move;
+	if (!proto.shuffle) proto.shuffle = ArrayExtensions.shuffle;
+	return proto;
+};
+
+export default extendsPrototype(Array.prototype);

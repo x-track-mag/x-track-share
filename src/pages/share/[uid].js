b050a401d0cfa2a1fbd14ee8f3ae5f5bab9057f2
zipang <christophe.Desguez@gmail.com>
2021-04-30 10:51:28 +0200

    CloudinaryExplorer provides a unique context to explore all shared folders and sub folders

--- src/pages/share/[uid].js
+++ src/pages/share/[uid].js
@@ -1,20 +1,26 @@
 import APIClient from "../../lib/services/APIClient.js";
-import CloudinaryFolder from "../../components/CloudinaryFolder.js";
+import CloudinaryExplorer from "../../components/CloudinaryExplorer.js";
 import { withEventBus } from "../../components/EventBusProvider.js";
 
 /**
  * Render a single share folder
  * @param {Object} props
  */
-const SharePage = (props) => <CloudinaryFolder {...props} />;
+const SharePage = (props) => <CloudinaryExplorer {...props} />;
 
 /**
  * Retrieve the folder content /share/${uid}
  */
 export const getServerSideProps = async ({ params }) => {
 	const uid = params.uid;
-	const props = await APIClient.get(`/api/share/${uid}`);
-	return { props };
+	const folders = await APIClient.get(`/api/share/${uid}`);
+
+	return {
+		props: {
+			folders,
+			path: uid
+		}
+	};
 };
 
 export default withEventBus(SharePage);

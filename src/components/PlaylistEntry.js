ac024dbf4726ac5702fa625c9f61b7c00b938379
zipang <christophe.Desguez@gmail.com>
2021-05-02 22:22:47 +0200

    Add an icon

--- src/components/PlaylistEntry.js
+++ src/components/PlaylistEntry.js
@@ -1,14 +1,18 @@
-import { Box } from "@chakra-ui/layout";
+import { Flex } from "@chakra-ui/layout";
+import { AspectRatio } from "@cloudinary/base/qualifiers/aspectRatio";
 
 /**
  * An entry inside the playlist, representing an audio or video asset to play
  * @param {PlaylistEntryProps} props
  * @returns
  */
-const PlaylistEntry = ({ title, format, url, asset_id }) => (
-	<Box bg="black" m="0" p="1rem" as="li" key={asset_id}>
-		{title}
-	</Box>
+const PlaylistEntry = ({ filename, format, url, asset_id }) => (
+	<Flex bg="black" m="0" as="li" key={asset_id} alignItems="center">
+		<AspectRatio ratio={1}>
+			<PlayPauseIcon />
+		</AspectRatio>
+		{filename}
+	</Flex>
 );
 
 export default PlaylistEntry;

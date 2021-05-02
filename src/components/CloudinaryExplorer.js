2bb1b94345ae0cb56ebee749edd490b26660d6c2
zipang <christophe.Desguez@gmail.com>
2021-05-02 22:24:17 +0200

    useShareContext()

--- src/components/CloudinaryExplorer.js
+++ src/components/CloudinaryExplorer.js
@@ -1,7 +1,22 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import CloudinaryFolder from "./CloudinaryFolder";
 
+/**
+ * @typedef SharedFolderContext
+ * @property {Object} folders
+ * @property {String} current Current path
+ * @property {Function} setCurrent Change the current folder path
+ */
+
 const ShareContext = createContext();
+
+/**
+ * useShareContext() Hook
+ * @return {SharedFolderContext}
+ */
+export const useShareContext = () => {
+	return useContext(ShareContext);
+};
 
 const CloudinaryExplorer = ({ folders, path = "" }) => {
 	const [current, setCurrent] = useState(path);

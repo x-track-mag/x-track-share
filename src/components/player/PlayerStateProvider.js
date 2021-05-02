7b502a7db60a569a8df3fa9e908e3c1b79927dd4
zipang <christophe.desguez@gmail.com>
2021-05-02 03:59:01 +0200

    Generic PlayerStateProvider whose logic is identical for audio or videos playlists

---
+++ src/components/player/PlayerStateProvider.js
@@ -0,0 +1,56 @@
+import { useState, createContext, useContext } from "react";
+import PlaylistEntry from "./PlaylistEntry.js";
+
+/**
+ * @typedef PlayerState
+ * @property {Array} playlist
+ * @property {Number} selected
+ * @property {Boolean} playing
+ * @property {Function} setPlayerState
+ */
+
+const PlayerContext = createContext();
+
+const PlayerStateProvider = ({ id, playlist, children }) => {
+	const [playerState, setPlayerState] = useState({
+		playlist: videos,
+		selected: 0,
+		playing: false // play/pause
+	});
+
+	return (
+		<PlayerContext.Provider value={{ setPlayerState, ...playerState }}>
+			{children}
+		</PlayerContext.Provider>
+	);
+};
+
+export default PlayerStateProvider;
+
+/**
+ * usePlayerState() Hook
+ * @return {PlayerState}
+ */
+export const usePlayerState = () => {
+	const ps = useContext(PlayerContext);
+	if (!ps) {
+		throw new Exception(`usePlayerState() called without a PlayerStateProvider`);
+	}
+	return ps;
+};
+
+export const Playlist = ({ playerId, playlist }) => {
+	const { selected } = usePlayerState();
+	return (
+		<Box as="ol" className="playlist-container">
+			{playlist.map((entry, i) => (
+				<PlaylistEntry
+					index={i}
+					playerId={playerId}
+					selected={i === selected}
+					{...entry}
+				/>
+			))}
+		</Box>
+	);
+};

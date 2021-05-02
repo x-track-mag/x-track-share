2fe6afbce228c37fa796710e5ecf277b74ca1d00
zipang <christophe.desguez@gmail.com>
2021-05-02 23:06:18 +0200

    changeTrack

--- src/components/player/PlaylistEntry.js
+++ src/components/player/PlaylistEntry.js
@@ -1,15 +1,28 @@
 import { Box } from "@chakra-ui/layout";
 import { useEventBus } from "../EventBusProvider.js";
 import clsx from "clsx";
+import { usePlayerState } from "./PlayerStateProvider.js";
 
 /**
  * An entry inside the playlist, representing an audio or video asset to play
  * @param {PlaylistEntryProps} props
  * @returns
  */
-const PlaylistEntry = ({ index, selected, playerId, title, format, url, asset_id }) => {
+const PlaylistEntry = ({
+	index,
+	selected,
+	playerId,
+	filename,
+	format,
+	url,
+	asset_id
+}) => {
 	const eb = useEventBus();
-	const playerState = usePlayerState();
+	// const playerState = usePlayerState();
+
+	const changeTrack = (i) => {
+		eb.emit(`${playerId}:changeTrack`, i);
+	};
 
 	return (
 		<Box
@@ -18,10 +31,10 @@ const PlaylistEntry = ({ index, selected, playerId, title, format, url, asset_id
 			p="1rem"
 			as="li"
 			key={asset_id}
-			onClick={changePlaying(selected)}
+			onClick={changeTrack(index)}
 			className={clsx({ selected })}
 		>
-			{title}
+			{filename}
 		</Box>
 	);
 };

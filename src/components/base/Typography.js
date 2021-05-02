bc0cf44e95224b113245562d3db0d8c065932b39
zipang <christophe.Desguez@gmail.com>
2021-05-02 22:21:47 +0200

    simplifications

--- src/components/base/Typography.js
+++ src/components/base/Typography.js
@@ -1,86 +1,18 @@
 import { Heading, Text } from "@chakra-ui/react";
 
 export const Title = ({ children, ...moreStyles }) => (
-	<Heading
-		as="h2"
-		fontSize="3.2rem"
-		lineHeight="3.9rem"
-		textAlign="center"
-		textTransform="uppercase"
-		mb="0.5rem"
-		{...moreStyles}
-	>
+	<Heading as="h2" fontSize="2rem" lineHeight="2.2rem" fontWeight={500} {...moreStyles}>
 		{children}
 	</Heading>
 );
 
 export const Subtitle = ({ children, ...moreStyles }) => (
-	<Heading
-		as="h3"
-		fontSize="3.2rem"
-		lineHeight="3.9rem"
-		textAlign="center"
-		{...moreStyles}
-	>
+	<Heading as="h3" fontSize="1.6rem" lineHeight="2rem" fontWeight={500} {...moreStyles}>
 		{children}
 	</Heading>
 );
 
-export const Message = ({ children }) => (
-	<chakra.span
-		fontFamily="Arachne"
-		fontSize="2.8rem"
-		fontWeight="600"
-		lineHeight="4rem"
-		textColor="brand.green"
-		whiteSpace="nowrap"
-		m="0"
-	>
-		{children}
-	</chakra.span>
-);
-
-export const Blockquote = ({ text }) => (
-	<chakra.blockquote
-		fontFamily="Arachne"
-		fontSize={["1rem", "1.5rem", "1.8rem"]}
-		lineHeight={["1.5rem", "2rem", "2.5rem"]}
-		textAlign="center"
-		width="100%"
-		padding="5rem 8rem"
-		border="solid black 5px"
-		borderRadius="100%"
-	>
-		{Array.isArray(text)
-			? text.map((paragraph, i) => <p key={`blocquote-p-${i}`}>{paragraph.text}</p>)
-			: { text }}
-	</chakra.blockquote>
-);
-
-export const Tag = ({ textColor, children }) => (
-	<Text
-		as="div"
-		fontFamily="PressGothicPro"
-		textColor={textColor}
-		fontSize="1.5rem"
-		lineHeight="1em"
-		margin="0.25em 0"
-		textAlign="center"
-		textTransform="uppercase"
-		width="100%"
-		padding="0.5rem 2rem 0.4rem"
-		border="solid 4px"
-		borderColor={textColor}
-		borderRadius="100%"
-	>
-		{children}
-	</Text>
-);
-
 export default {
 	Title,
-	Subtitle,
-	Message,
-	Blockquote,
-	Tag
+	Subtitle
 };

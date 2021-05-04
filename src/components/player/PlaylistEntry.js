ff71f9a9274c2052c716bced19376d5bd189d1cc
zipang <christophe.Desguez@gmail.com>
2021-05-04 14:37:52 +0200

    Send play/pause or changeTrack events

--- src/components/player/PlaylistEntry.js
+++ src/components/player/PlaylistEntry.js
@@ -2,39 +2,48 @@ import { Box } from "@chakra-ui/layout";
 import { useEventBus } from "../EventBusProvider.js";
 import clsx from "clsx";
 import { usePlayerState } from "./PlayerStateProvider.js";
+import PlayPauseIcon from "./PlayPauseIcon.js";
 
 /**
  * An entry inside the playlist, representing an audio or video asset to play
  * @param {PlaylistEntryProps} props
  * @returns
  */
-const PlaylistEntry = ({
-	index,
-	selected,
-	playerId,
-	filename,
-	format,
-	url,
-	asset_id
-}) => {
+const PlaylistEntry = ({ index, playerId, filename, format, url, asset_id }) => {
 	const eb = useEventBus();
-	// const playerState = usePlayerState();
+	const { selected, playing } = usePlayerState();
 
 	const changeTrack = (i) => {
-		eb.emit(`${playerId}:changeTrack`, i);
+		if (index === selected) {
+			if (playing) {
+				eb.emit(`${playerId}:pause`, i);
+			} else {
+				eb.emit(`${playerId}:play`, i);
+			}
+		} else {
+			eb.emit(`${playerId}:changeTrack`, i);
+		}
 	};
 
 	return (
 		<Box
 			bg="black"
 			m="0"
-			p="1rem"
+			p="0.1rem 2rem"
 			as="li"
 			key={asset_id}
-			onClick={changeTrack(index)}
+			_hover={{ cursor: "pointer" }}
+			onClick={() => changeTrack(index)}
 			className={clsx({ selected })}
 		>
-			{filename}
+			<PlayPauseIcon
+				show={index === selected}
+				playing={index === selected && playing}
+				size="2rem"
+			/>
+			<Box as="span" ml="1rem">
+				{filename}
+			</Box>
 		</Box>
 	);
 };

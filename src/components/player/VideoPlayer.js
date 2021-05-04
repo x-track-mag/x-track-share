633eb080b6e7652d66083084406b32bbc9037e1c
zipang <christophe.Desguez@gmail.com>
2021-05-04 14:40:23 +0200

    Register events : play/pause/changeTrack

--- src/components/player/VideoPlayer.js
+++ src/components/player/VideoPlayer.js
@@ -1,4 +1,4 @@
-import { AspectRatio } from "@chakra-ui/layout";
+import { Box, Flex } from "@chakra-ui/layout";
 import { useEffect } from "react";
 import { useEventBus } from "../EventBusProvider";
 import { usePlayerState } from "./PlayerStateProvider";
@@ -6,58 +6,127 @@ import { Cloudinary } from "cloudinary-core";
 import "cloudinary-video-player/dist/cld-video-player.light.min";
 import "cloudinary-video-player/dist/cld-video-player.light.min.css";
 
+/**
+ * Instantiate a custom CloudinaryPlayer
+ * @param {String} id
+ * @param {Array<String>} playlist
+ */
 const createPlayer = (id, playlist) => {
-	const cloudinary = new Cloudinary({
-		cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-		secure: true
-	});
-	const player = cloudinary.videoPlayer(id, {
-		fluid: true,
-		controls: true,
-		showJumpControls: true,
-		fontFace: "Audiowide",
-		colors: {
-			accent: "#0000ff",
-			text: "#ffff00"
-		},
-		logoOnclickUrl: "https://x-track.net",
-		logoImageUrl:
-			"https://res.cloudinary.com/x-track/image/fetch/h_25/https://x-track.net/img/x-track-logo.png",
-		floatingWhenNotVisible: "right",
-		posterOptions: {
-			transformation: {
-				startOffset: "0"
-			}
-		}
-	});
-
-	if (playlist && playlist.length) {
-		player.source(playlist([0].url), {
-			sourceTypes: ["hls"]
+	try {
+		console.log(
+			`Creating player for ${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}`
+		);
+		const cloudinary = new Cloudinary({
+			cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+			secure: true
 		});
-	}
+		const player = cloudinary.videoPlayer(id, {
+			fluid: false,
+			controls: true,
+			bigPlayButton: true,
+			showJumpControls: true,
+			fontFace: "inherit",
+			colors: {
+				accent: "#0000ff",
+				text: "#ffff00"
+			},
+			logoOnclickUrl: "https://x-track.net",
+			logoImageUrl:
+				"https://res.cloudinary.com/x-track/image/fetch/h_25/https://x-track.net/img/x-track-logo.png",
+			floatingWhenNotVisible: "right",
+			posterOptions: {
+				transformation: {
+					startOffset: "0"
+				}
+			}
+		});
 
-	return player;
+		["play", "pause"].map((methodName) => {
+			player[methodName] = player[methodName].bind(player);
+		});
+
+		if (playlist && playlist.length) {
+			player.playlist(
+				playlist.map((p) => p.url),
+				{
+					sourceTypes: ["hls"]
+				}
+			);
+		}
+
+		return player;
+	} catch (err) {
+		console.error(err);
+	}
 };
 
-const VideoPlayer = ({ id }) => {
+const registerPlayerEvents = (playerId, player, eb, merge) => {
+	if (!player) return;
+	eb.on(`${playerId}:play`, player.play);
+	eb.on(`${playerId}:pause`, player.pause);
+	eb.on(`${playerId}:changeTrack`, (selected) => {
+		player.playlist().playAtIndex(selected);
+	});
+	// Now the player events inform us of the real state changes
+	player.on("play", () => {
+		merge({
+			selected: player.playlist().currentIndex(),
+			playing: true
+		});
+	});
+	player.on("pause", () => {
+		merge({
+			selected: player.playlist().currentIndex(),
+			playing: false
+		});
+	});
+	player.on("sourcechanged", () => {
+		merge({
+			selected: player.playlist().currentIndex()
+		});
+	});
+};
+const unregisterPlayerEvents = (playerId, player, eb) => {
+	if (!player) return;
+	eb.off(`${playerId}:play`, player.play);
+	eb.off(`${playerId}:pause`, player.pause);
+	eb.off(`${playerId}:next`, player.playNext);
+	eb.off(`${playerId}:previous`, player.playPrevious);
+};
+
+/**
+ *
+ * @param {VideoPlayerProps} props
+ */
+const VideoPlayer = ({ id, playlist }) => {
 	let player; // the CloudinaryPlayer instance
-	const { playlist, selected } = usePlayerState();
+	const { selected, merge } = usePlayerState();
 	let eb = useEventBus();
 
-	// Load the source
+	// Instanciate the player
 	useEffect(() => {
+		console.log(`Creating video player ${id}`);
 		player = createPlayer(id, playlist);
+		registerPlayerEvents(id, player, eb, merge);
 
 		return () => {
+			console.log(`Unregistrating video player ${id}`);
+			unregisterPlayerEvents(id, player, eb);
 			player = null;
 		};
 	}, [id]);
 
 	return (
-		<AspectRatio className="video-container" ratio={4 / 3} height="100%">
-			<video controls className="video-player" id={id} width="100%" height="100%" />
-		</AspectRatio>
+		<Box
+			className="video-container"
+			width="100%"
+			height="40vh"
+			padding="0 2rem"
+			position="relative"
+			overflow="hidden"
+		>
+			<video controls className="video-player" id={id} height="100%" width="100%" />
+		</Box>
 	);
 };
 

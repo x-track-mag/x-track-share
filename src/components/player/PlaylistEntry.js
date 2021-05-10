b3b27249ff02766c4160be089e9b3df2dca65312
zipang <christophe.Desguez@gmail.com>
2021-05-10 12:03:25 +0200

    Disply the track duration

--- src/components/player/PlaylistEntry.js
+++ src/components/player/PlaylistEntry.js
@@ -4,17 +4,28 @@ import clsx from "clsx";
 import { usePlayerState } from "./PlayerStateProvider.js";
 import PlayPauseIcon from "./PlayPauseIcon.js";
 
+const formatDuration = (ms) => `${Math.floor(ms / 60)}:${Math.round(ms % 60)}`;
+
 /**
  * An entry inside the playlist, representing an audio or video asset to play
  * @param {PlaylistEntryProps} props
  * @returns
  */
-const PlaylistEntry = ({ index, playerId, filename, format, url, asset_id }) => {
+const PlaylistEntry = ({
+	index,
+	playerId,
+	filename,
+	format,
+	duration,
+	url,
+	asset_id
+}) => {
 	const eb = useEventBus();
-	const { selected, playing } = usePlayerState();
+	const { selectedIndex, playing } = usePlayerState();
+	const selected = selectedIndex === index;
 
 	const changeTrack = (i) => {
-		if (index === selected) {
+		if (selected) {
 			if (playing) {
 				eb.emit(`${playerId}:pause`, i);
 			} else {
@@ -27,22 +38,23 @@ const PlaylistEntry = ({ index, playerId, filename, format, url, asset_id }) => 
 
 	return (
 		<Box
+			role="group"
 			bg="black"
 			m="0"
 			p="0.1rem 2rem"
 			as="li"
 			key={asset_id}
-			_hover={{ cursor: "pointer" }}
+			// _hover={{ cursor: "pointer", color: "yellow" }}
+			// color={selected ? "yellow" : "white"}
 			onClick={() => changeTrack(index)}
-			className={clsx({ selected })}
+			className={clsx("playlist_entry", { selected })}
 		>
-			<PlayPauseIcon
-				show={index === selected}
-				playing={index === selected && playing}
-				size="2rem"
-			/>
+			<PlayPauseIcon show={selected} playing={selected && playing} size="2rem" />
 			<Box as="span" ml="1rem">
 				{filename}
+			</Box>
+			<Box as="span" ml="1rem">
+				{formatDuration(duration)}
 			</Box>
 		</Box>
 	);

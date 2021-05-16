544f165be3e3bf1f3e18463f7fe6a7f5dd6285de
zipang <christophe.Desguez@gmail.com>
2021-05-16 17:43:14 +0200

    JS Doc

--- src/components/ShareContextProvider.js
+++ src/components/ShareContextProvider.js
@@ -22,17 +22,25 @@ export const withShareContext = (Component) => ({ folders, path, props }) => {
 	const [current, setCurrent] = useState(path);
 	const router = useRouter();
 
+	/**
+	 * Use this to navigate between folders inside a shared context root
+	 * @param {String} path
+	 */
+	const navigate = (path) => (evt) => {
+		evt.preventDefault();
+		router.push(`/share/${path}`, undefined, { shallow: true });
+	};
+
+	/**
+	 * Detect the path changes to set the new current path inside the shared context root
+	 * @param {String} path
+	 */
 	const handlePathChange = (path) => {
 		const sharedPath = path.replace("/share/", "");
 		console.log(`router handlePathChange() : ${current} => ${sharedPath} `);
 		if (sharedPath !== current) {
 			setCurrent(sharedPath);
 		}
-	};
-
-	const navigate = (path) => (evt) => {
-		evt.preventDefault();
-		router.push(`/share/${path}`, undefined, { shallow: true });
 	};
 
 	useEffect(() => {

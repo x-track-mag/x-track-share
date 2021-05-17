600acccda36ba92a139f06993ee9fdaa34173b5d
zipang <christophe.Desguez@gmail.com>
2021-05-17 18:20:05 +0200

    VScrollPositionProvider gives the scroll position and its direction

---
+++ src/components/base/VScrollPositionProvider.js
@@ -0,0 +1,87 @@
+import {
+	createContext,
+	useContext,
+	useState,
+	useEffect,
+	useLayoutEffect,
+	useRef
+} from "react";
+
+const VScrollPositionContext = createContext();
+
+const INITIAL_POSITION = {
+	scrollY: 0,
+	direction: "none"
+};
+
+/**
+ * NOTE : Don't forget the {children} when writing a context provider !
+ * @param props
+ * @param
+ */
+const VScrollPositionProvider = ({ children }) => {
+	// Save current viewport size in the state object
+	let [vscrollPosition, setVScrollPosition] = useState(INITIAL_POSITION);
+	const previous = useRef();
+
+	useEffect(() => {
+		previous.current = vscrollPosition;
+	});
+
+	// This useLayoutEffect will execute only once because
+	// it does not have any dependencies.
+	useLayoutEffect(() => {
+		// Listen to window scroll event
+		const measureVScrollPosition = () => {
+			requestAnimationFrame(() => {
+				const updated = {
+					scrollY: window.scrollY,
+					direction: window.scrollY >= previous.current.scrollY ? "down" : "up"
+				};
+
+				setVScrollPosition(updated);
+			});
+		};
+		window.addEventListener("scroll", measureVScrollPosition);
+
+		// return the clean up function
+		return () => {
+			window.removeEventListener("scroll", measureVScrollPosition);
+		};
+	}, []);
+
+	return (
+		<VScrollPositionContext.Provider value={vscrollPosition}>
+			{children}
+		</VScrollPositionContext.Provider>
+	);
+};
+
+/**
+ * React Hook to retrieve an up-to-date version of the vertical scroll position
+ * @return {Number}
+ */
+export const useVScrollPosition = () => {
+	const vscrollPosition = useContext(VScrollPositionContext);
+	if (vscrollPosition === undefined) {
+		throw new Error(
+			`useVScrollPosition() hook can only be used from inside a <VScrollPositionProvider/> parent`
+		);
+	}
+	return vscrollPosition;
+};
+
+/**
+ * Build a Higher Order Component that will allways receive
+ * an updated `vscrollPosition` prop with the position and direction of the scroll
+ * @param {JSX.Element} Component
+ */
+export const withVScrollPosition = (Component) => (props) => {
+	return (
+		<VScrollPositionProvider>
+			<Component {...props} />
+		</VScrollPositionProvider>
+	);
+};
+
+export default VScrollPositionProvider;

ce1c0dc48d8f1ca7fa3277dd2f17a7f16f48ee28
zipang <christophe.Desguez@gmail.com>
2021-05-26 20:22:48 +0200

    Removed the localStorage access

--- src/pages/share/[...uid].js
+++ src/pages/share/[...uid].js
@@ -3,31 +3,6 @@ import { withEventBus } from "../../components/EventBusProvider.js";
 import CloudinaryFolder from "../../components/CloudinaryFolder.js";
 import { withShareContext } from "../../components/ShareContextProvider.js";
 
-const retrieveFromCache = (uid) => {
-	if (typeof window === "undefined") return;
-	let cached = window.localStorage.getItem(uid);
-	if (!cached) return null;
-	cached = JSON.parse(cached);
-	if (cached.timeStamp > Date.now() - 10 * 60 * 1000) {
-		// expired
-		return null;
-	} else {
-		console.log(`Loaded from cache`, cached);
-		return cached;
-	}
-};
-
-const storeInCache = (uid, data) => {
-	if (typeof window === "undefined") return;
-	window.localStorage.setItem(
-		uid,
-		JSON.stringify({
-			...data,
-			timeStamp: Date.now()
-		})
-	);
-};
-
 /**
  * Retrieve the folder content /share/${uid}
  */
@@ -35,16 +10,12 @@ export const getServerSideProps = async ({ params }) => {
 	const uid = params.uid;
 	console.log(`getServerSideProps(${uid.join("/")})`);
 
-	let folders = retrieveFromCache(uid);
-
-	if (!folders) {
-		folders = await APIClient.get(`/api/share/${uid[0]}`);
-		storeInCache(uid, folders);
-	}
+	const { folders, sharedOptions } = await APIClient.get(`/api/share/${uid[0]}`);
 
 	return {
 		props: {
 			folders,
+			sharedOptions,
 			path: uid.join("/")
 		}
 	};

a40309623853af1f81945076fa3fbc47700c72c7
zipang <christophe.Desguez@gmail.com>
2021-05-26 20:26:35 +0200

    Added the sharedOptions in the ShareContext and renamed a bunch of things

--- src/components/ShareContextProvider.js
+++ src/components/ShareContextProvider.js
@@ -8,7 +8,7 @@ import SharedFolder from "../lib/cloudinary/SharedFolder";
  * @property {Object} folders the navigable shared folders
  * @property {String} current Current path
  * @property {SharedFolder} playlist A virtual folder containing what the user choosed to add to
- * @property {Function} setCurrent Change the current folder path
+ * @property {Function} navigate Change the current folder path
  */
 
 const ShareContext = createContext();
@@ -21,24 +21,30 @@ export const useShareContext = () => {
 	return useContext(ShareContext);
 };
 
-export const withShareContext = (Component) => ({ folders, path, props }) => {
+export const withShareContext = (Component) => ({
+	folders,
+	sharedOptions,
+	path,
+	props
+}) => {
 	const sharedRoot = path.split("/")[0];
-	Object.keys(folders).forEach((key) => (folders[key].displayPlaylist = true));
-	const [playlist] = useState(
-		new SharedFolder(`${sharedRoot}/playlist`, {
+
+	// There is an option to add selected tracks to a virtual playlist
+	const [selectedTracks] = useState(
+		new SharedFolder(`${sharedRoot}/selected-tracks`, {
 			hidden: true,
-			displayPlaylist: false
+			displaySelectedTracks: false
 		})
 	);
 	const [current, setCurrent] = useState(path);
 	const router = useRouter();
 	const eb = useEventBus();
 
-	const addToPlaylist = (track) => {
-		playlist.addMedia(track);
+	const addToSelectedTracks = (track) => {
+		selectedTracks.addMedia(track);
 	};
-	const removeFromPlaylist = (track) => {
-		playlist.removeMedia(track);
+	const removeFromSelectedTracks = (track) => {
+		selectedTracks.removeMedia(track);
 	};
 
 	/**
@@ -47,7 +53,7 @@ export const withShareContext = (Component) => ({ folders, path, props }) => {
 	 */
 	const navigate = (path) => (evt) => {
 		evt.preventDefault();
-		router.push(`/share/${path}`, undefined, { shallow: true });
+		router.push(`/share/${path}`, undefined, { shallow: true }); // shallow TRUE will not call getServerSideProps
 	};
 
 	/**
@@ -63,19 +69,21 @@ export const withShareContext = (Component) => ({ folders, path, props }) => {
 	};
 
 	useEffect(() => {
-		eb.on("playlist:add", addToPlaylist);
-		eb.on("playlist:remove", removeFromPlaylist);
+		eb.on("selected-tracks:add", addToSelectedTracks);
+		eb.on("selected-tracks:remove", removeFromSelectedTracks);
 		router.events.on("routeChangeComplete", handlePathChange);
 		return () => {
 			console.log(`Unregistering SharedContext event handler`);
-			eb.off("playlist:add", addToPlaylist);
-			eb.off("playlist:remove", removeFromPlaylist);
+			eb.off("selected-tracks:add", addToSelectedTracks);
+			eb.off("selected-tracks:remove", removeFromSelectedTracks);
 			router.events.off("routeChangeComplete", handlePathChange);
 		};
 	}, [current]);
 
 	return (
-		<ShareContext.Provider value={{ folders, current, playlist, navigate }}>
+		<ShareContext.Provider
+			value={{ folders, sharedOptions, current, selectedTracks, navigate }}
+		>
 			<Component {...props} />
 		</ShareContext.Provider>
 	);

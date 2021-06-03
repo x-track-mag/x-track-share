6cc5736f43dd40848bcb67ff0a04111e3cd7db2c
zipang <christophe.Desguez@gmail.com>
2021-06-03 08:52:14 +0200

    Form inputs based on Chakra UI

---
+++ src/components/forms/inputs/Text.js
@@ -0,0 +1,110 @@
+import { FormLabel } from "@chakra-ui/form-control";
+import { FormHelperText } from "@chakra-ui/form-control";
+import { FormErrorMessage } from "@chakra-ui/form-control";
+import { FormControl } from "@chakra-ui/form-control";
+import { Input } from "@chakra-ui/input";
+import React, { createRef, useEffect, useLayoutEffect } from "react";
+import { useFormValidationContext } from "../validation/FormValidationProvider.js";
+import { evalContextualProp } from "../validation/utils.js";
+
+/**
+ * @typedef TextInputProps
+ * @property {String}  name The name of the field (can use dots to describe a deeply nested property)
+ * @property {String}  label The field label
+ * @property {Boolean} [required=false] is this field value required ?
+ * @property {String}  [defaultValue] Default value is returned if nothing was entered
+ * @property {String}  [placeHolder=""] Text shown when no input or default value is given
+ * @property {String}  [helperText=""]  Helper text is shown behind the input field
+ * @property {Boolean} [autoFocus=false] Get focus on this field on page load ?
+ * @property {Boolean} [readOnly=false] Can or cannot edit
+ * @property {Object}  [validation={}] Validation object
+ */
+
+/**
+ * Basically same features as Text input
+ * but with validation
+ * @param {TextInputProps} props -
+ */
+const Text = ({
+	name = "text",
+	label,
+	placeHolder,
+	helperText,
+	required = false,
+	disabled = false,
+	defaultValue,
+	autoComplete = false,
+	autoFocus = false,
+	readOnly = false,
+	validation = {},
+	...moreProps
+}) => {
+	// Find the form validation context to register our input
+	const inputRef = createRef();
+	const {
+		register,
+		data,
+		errors,
+		setData,
+		getData,
+		validate
+	} = useFormValidationContext();
+
+	const errorMessage = errors[name]?.message || "";
+
+	if (!label) label = name;
+	// Pass the required attribute to the validation object
+	if (evalContextualProp(data, required)) label += "*";
+
+	// Keep form context data in sync
+	const onChange = (evt) => {
+		setData(name, evt.target.value);
+		if (errorMessage) {
+			validate(name); // Revalidate immediately to show when the input becomes valid again
+		}
+	};
+
+	// Register our Input so that the validation can take effect
+	register(name, { inputRef, required, defaultValue, validation });
+
+	useEffect(() => {
+		console.log(`Re-rendering text field ${name}`);
+	});
+
+	useLayoutEffect(() => {
+		inputRef.current.value = getData(name); // Apply the default value
+		if (autoFocus || errorMessage) {
+			console.log(`Focus on ${name} (${errorMessage})`);
+			inputRef.current.focus();
+		}
+	}, [name]);
+
+	return (
+		<FormControl
+			id={name}
+			onChange={onChange}
+			autoComplete={autoComplete ? "" : "off"}
+			isInvalid={Boolean(errorMessage)}
+			isDisabled={evalContextualProp(data, disabled)}
+			isReadOnly={readOnly}
+			{...moreProps}
+		>
+			<FormLabel>{label}</FormLabel>
+			<Input
+				name={name}
+				ref={inputRef}
+				variant="solid"
+				height="2rem"
+				borderColor="yellow"
+				bgColor="black"
+				color="yellow"
+				autoFocus={autoFocus}
+				placeholder={placeHolder}
+			/>
+			<FormErrorMessage>{errorMessage}</FormErrorMessage>
+			<FormHelperText>{helperText}</FormHelperText>
+		</FormControl>
+	);
+};
+
+export default Text;
---
+++ src/components/forms/inputs/Text.stories.js
@@ -0,0 +1,79 @@
+// Text.stories.js
+import Text from "./Text.js";
+import {
+	useFormValidationContext,
+	FormValidationProvider
+} from "../validation/FormValidationProvider.js";
+
+const VForm = ({ children, ...props }) => {
+	const { validate } = useFormValidationContext();
+
+	// Just log what's goin on when submitting
+	const onSubmit = (evt) => {
+		evt.preventDefault();
+		validate({
+			onSuccess: console.log,
+			onError: console.error
+		});
+	};
+
+	return (
+		<form onSubmit={onSubmit} {...props}>
+			{children}
+			<input type="submit" className="hidden" aria-hidden="true" />
+		</form>
+	);
+};
+
+// This default export determines where your story goes in the story list
+export default {
+	title: "Text Input with validation",
+	component: Text,
+	args: {
+		required: false,
+		autoFocus: true,
+		readOnly: false,
+		defaultValue: ""
+	},
+	argTypes: {
+		required: { control: { type: "boolean" } },
+		autoFocus: { control: { type: "boolean" } },
+		readOnly: { control: { type: "boolean" } },
+		defaultValue: { control: { type: "text" } }
+	}
+};
+
+export const SimpleText = ({ defaultValue, ...args }) => (
+	<FormValidationProvider>
+		<VForm id="simple-text-form">
+			<Text
+				{...args}
+				defaultValue={defaultValue}
+				helperText="With default value"
+				name="firstName"
+				label="Prénom"
+			/>
+			<Text
+				{...args}
+				placeHolder="Valjean"
+				helperText="With place holder"
+				name="lastName"
+				label="Nom"
+			/>
+		</VForm>
+	</FormValidationProvider>
+);
+SimpleText.args = {
+	defaultValue: "Jean"
+};
+
+export const ReadOnlyTextWithData = ({ ...args }) => (
+	<FormValidationProvider data={{ firstName: "John" }}>
+		<VForm id="simple-text-form">
+			<Text {...args} name="firstName" label="Prénom" />
+		</VForm>
+	</FormValidationProvider>
+);
+ReadOnlyTextWithData.args = {
+	readOnly: true
+};

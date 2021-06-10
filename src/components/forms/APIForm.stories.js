f460b45ca19ab9b40a47df3efe33fa6a7b372e6f
zipang <christophe.Desguez@gmail.com>
2021-06-10 23:51:53 +0200

    Use fields validation

--- src/components/forms/APIForm.stories.js
+++ src/components/forms/APIForm.stories.js
@@ -2,14 +2,30 @@ import { Input } from "@chakra-ui/input";
 import { Box } from "@chakra-ui/layout";
 import APIForm from "./APIForm.js";
 import Text from "./inputs/Text.js";
+import Email from "./inputs/Email.js";
 
 export const SendMailReport = () => (
 	<Box w="20rem" h="100%">
 		<APIForm method="POST" action="/api/mailreport">
-			<Text name="fullName" placeHolder="John DOE" />
-			<Text name="email" placeHolder="john.doe@x.org" />
+			<Text
+				name="fullName"
+				placeHolder="John DOE"
+				autoFocus={true}
+				required={true}
+			/>
+			<Email name="email" placeHolder="john.doe@x.org" required={true} />
 			<Text name="message" placeHolder="Hello" />
-			<Input type="submit" value="Envoyer" bgColor="lightgray" color="blue" />
+			<Input
+				type="submit"
+				value="Envoyer"
+				mt={4}
+				borderRadius={0}
+				_focus={{
+					borderColor: "yellow"
+				}}
+				bgColor="blue"
+				color="yellow"
+			/>
 		</APIForm>
 	</Box>
 );

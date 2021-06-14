cb3899efb3a55b1bf9ec6b207a4b075c29e6f309
zipang <christophe.Desguez@gmail.com>
2021-06-14 23:44:59 +0200

    Display a drawer  to show the download form

--- src/components/SharedFolderNavigation.js
+++ src/components/SharedFolderNavigation.js
@@ -1,10 +1,10 @@
+import { useLayoutEffect } from "react";
+import { useDisclosure } from "@chakra-ui/hooks";
 import { Box, Heading } from "@chakra-ui/layout";
-import Breadcrumbs from "./Breadcrumbs";
-import { useShareContext } from "./ShareContextProvider";
-
-const download = (tracks, displayDownloadForm) => (evt) => {
-	evt.preventDefault();
-};
+import { Drawer, DrawerOverlay, DrawerHeader, DrawerContent } from "@chakra-ui/modal";
+import Breadcrumbs from "./Breadcrumbs.js";
+import DownloadForm from "./forms/DownloadForm.js";
+import { useShareContext } from "./ShareContextProvider.js";
 
 const SharedFolderNavigation = ({ path }) => {
 	const {
@@ -12,11 +12,56 @@ const SharedFolderNavigation = ({ path }) => {
 		selectedTracks,
 		sharedOptions: { addToSelection, displayDownloadForm }
 	} = useShareContext();
+
+	// These variables will control the drawer to show the Download form
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	const callbacks = {};
+	/**
+	 * The callback function is static and can be referenced by every components on mount
+	 * But the real callbacks are dynamics and created later
+	 * @param {String} method Name of the callback to invoke
+	 * @return {Function}
+	 */
+	const callback = (method) => (...args) => {
+		console.log(`Calling ${method} on `, callbacks);
+		if (typeof callbacks[method] === "function") {
+			callbacks[method](...args);
+		} else {
+			console.error(`callbacks[${method}] is not defined. Why ?`);
+		}
+	};
+
+	useLayoutEffect(() => {
+		// Show the drawer and resolve when the API call to submit the form data ha been called
+		const showDownloadForm = () =>
+			new Promise((resolve, reject) => {
+				callbacks.onSuccess = (data) => {
+					onClose();
+					resolve(data);
+				};
+				callbacks.onError = reject;
+				console.log(`Show the download form !`);
+				onOpen();
+			});
+		callbacks.download = async (evt) => {
+			evt.preventDefault();
+			// Display the download form and get the download URL
+			const downloadUrl = await showDownloadForm().catch(alert);
+		};
+		console.log("Defined callbacks", callbacks);
+	}, [path]);
+
 	return (
 		<Box className="navigation-header" as="header">
 			<Breadcrumbs path={path} />
 			{addToSelection && selectedTracks.path !== path && (
-				<Heading display="inline-block" float="right" key={selectedTracks.path}>
+				<Heading
+					className="neon-text"
+					display="inline-block"
+					float="right"
+					key={selectedTracks.path}
+				>
 					<a
 						href={`/share/${selectedTracks.path}`}
 						onClick={navigate(selectedTracks.path)}
@@ -26,14 +71,35 @@ const SharedFolderNavigation = ({ path }) => {
 				</Heading>
 			)}
 			{addToSelection && selectedTracks.path === path && (
-				<Heading display="inline-block" float="right" key={selectedTracks.path}>
-					<a
-						href={`/share/${selectedTracks.path}/download`}
-						onClick={download(selectedTracks, displayDownloadForm)}
+				<>
+					<Heading
+						className="neon-text"
+						display="inline-block"
+						float="right"
+						key={selectedTracks.path}
 					>
-						Download
-					</a>
-				</Heading>
+						<a
+							href={`/share/${selectedTracks.path}/download`}
+							onClick={callback("download")}
+						>
+							Download
+						</a>
+					</Heading>
+					{displayDownloadForm && (
+						<Drawer isOpen={isOpen} onClose={onClose}>
+							<DrawerOverlay />
+							<DrawerHeader mt="50px">Merci de renseigner</DrawerHeader>
+							<DrawerContent padding={2} bgColor="black">
+								<DownloadForm
+									maxWidth="40rem"
+									selectedTracks={selectedTracks}
+									onSuccess={callback("onSuccess")}
+									onError={callback("onError")}
+								/>
+							</DrawerContent>
+						</Drawer>
+					)}
+				</>
 			)}
 		</Box>
 	);

313d02bef7e2139f12e5022cb03568834f99fbe3
zipang <christophe.Desguez@gmail.com>
2021-06-17 13:14:39 +0200

    useIsomorphicLayoutEffect (renamed)

--- src/hooks/useIsomorphicLayoutEffect.js
+++ src/hooks/useIsomorphicLayoutEffect.js
@@ -4,6 +4,7 @@ import { useEffect, useLayoutEffect } from "react";
  * Allow React not to complain when we want to use a Layout effect on client side
  * and not on SSR
  */
-const useUnivLayoutEffect = typeof window === "undefined" ? useEffect : useLayoutEffect;
+const useIsomorphicLayoutEffect =
+	typeof window === "undefined" ? useEffect : useLayoutEffect;
 
-export default useUnivLayoutEffect;
+export default useIsomorphicLayoutEffect;

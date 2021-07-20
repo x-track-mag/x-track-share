4f90e433f11eee05cde79e53b330cc1a1ebeac35
zipang <christophe.Desguez@gmail.com>
2021-07-20 22:06:31 +0200

    New SVG icons

---
+++ src/components/icons/SvgBin.js
@@ -0,0 +1,19 @@
+import Icon from "@chakra-ui/icon";
+
+/**
+ * @typedef SvgBinProps
+ * @property {CSSColor} [color="inherit"]
+ * @property {CSSSize} [size="18px"] Size of the rendered viewport
+ */
+
+/**
+ * Render an equilateral triangle pointing up, down, right or left
+ * @param {SvgBinProps} props
+ */
+const SvgBin = ({ color = "inherit", size = "18px", ...props }) => (
+	<Icon height={size} width={size} viewBox="0 0 32 32" {...props}>
+		<path fill={color} d="m 7,27 h 18 v -19 h 3 v -3 l -9,-1 h -6 l -9,1 v 3 h 3 z" />
+	</Icon>
+);
+
+export default SvgBin;

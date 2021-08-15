f9867a52b0558eb6951336ca4a3b834f41eb0d6c
zipang <christophe.Desguez@gmail.com>
2021-08-15 22:39:19 +0200

    100vh

--- src/components/base/Main.js
+++ src/components/base/Main.js
@@ -1,7 +1,7 @@
 import { Box } from "@chakra-ui/react";
 
 const Main = ({ children, ...props }) => (
-	<Box as="main" pt="3rem" w="100%" h="100%" position="relative" {...props}>
+	<Box as="main" pt="3rem" w="100%" h="100vh" position="relative" {...props}>
 		{children}
 	</Box>
 );

987c02a69b5f9bae595b5e0b6957ef3a500a2493
zipang <christophe.Desguez@gmail.com>
2021-08-21 20:03:48 +0200

    Test page for file uploads

--- src/pages/_admin/upload/index.js
+++ src/pages/_admin/upload/index.js
@@ -1,24 +1,6 @@
 import FileUpload from "../../../components/forms/inputs/FileUpload.js";
 import withAuth from "../../../components/auth/withAuth.js";
 
-function uploadFile(file) {
-	let url = "YOUR URL HERE";
-	let formData = new FormData();
-
-	formData.append("file", file);
-
-	fetch(url, {
-		method: "POST",
-		body: formData
-	})
-		.then(() => {
-			/* Done. Inform the user */
-		})
-		.catch(() => {
-			/* Error. Inform the user */
-		});
-}
-
-const UploadPage = () => <FileUpload />;
+const UploadPage = () => <FileUpload upload_url="/api/upload" />;
 
 export default withAuth(UploadPage);

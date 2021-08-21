04e86a176923961ddc22e3b60cc35aadc599dfc3
zipang <christophe.Desguez@gmail.com>
2021-08-21 20:05:30 +0200

    Auth services icons

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -3,5 +3,5 @@ export { default as GithubLogo } from "./SvgGithubLogo.js";
 export { default as GoogleLogo } from "./SvgGoogleLogo.js";
 export { default as MicrosoftLogo } from "./SvgMicrosoftLogo.js";
 export { default as Triangle } from "./SvgTriangle.js";
-export { default as Upload } from "./SvgUpload.js";
+export { default as UploadIcon } from "./SvgUpload.js";
 export { default as XtrackLogo } from "./SvgLogo.js";

755ddf60dbb78e59e9548abc314086c209d2dd8e
zipang <christophe.Desguez@gmail.com>
2021-09-03 00:19:28 +0200

    export default

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -6,3 +6,26 @@ export { default as Triangle } from "./SvgTriangle.js";
 export { default as UploadIcon } from "./SvgUpload.js";
 export { default as XtrackLogo } from "./SvgLogo.js";
 export { default as PlusIcon } from "./SvgPlus.js";
+
+const Icon = ({ name, ...props }) => {
+	switch (name) {
+		case "Bin":
+			return <Bin {...props} />;
+		case "GithubLogo":
+			return <GithubLogo {...props} />;
+		case "GoogleLogo":
+			return <GoogleLogo {...props} />;
+		case "MicrosoftLogo":
+			return <MicrosoftLogo {...props} />;
+		case "Triangle":
+			return <Triangle {...props} />;
+		case "UploadIcon":
+			return <UploadIcon {...props} />;
+		case "PlusIcon":
+			return <PlusIcon {...props} />;
+
+		default:
+			return null;
+	}
+};
+export default Icon;

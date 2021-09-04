5c18b10cb2373c9c151278bf35a3d7d38f880eb9
zipang <christophe.Desguez@gmail.com>
2021-09-04 20:30:39 +0200

    Import the code from CloudinaryFolder into the page. It's not a reusable component !

--- src/pages/share/[...uid].js
+++ src/pages/share/[...uid].js
@@ -1,6 +1,14 @@
+import { Grid, Stack } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import Folder from "../../components/base/Folder.js";
+import AudioPlaylistPlayer from "../../components/player/AudioPlaylistPlayer.js";
+import VideoPlaylistPlayer from "../../components/player/VideoPlaylistPlayer.js";
+import {
+	useSharedFolderContext,
+	withSharedFolderContext
+} from "../../components/SharedFolderContext.js";
+import SharedFolderNavigation from "../../components/SharedFolderNavigation.js";
 import APIClient from "../../lib/services/APIClient.js";
-import CloudinaryFolder from "../../components/CloudinaryFolder.js";
-import { withSharedFolderContext } from "../../components/SharedFolderContext.js";
 
 /**
  * Retrieve the folder content /share/${uid}
@@ -9,15 +17,77 @@ export const getServerSideProps = async ({ params }) => {
 	const uid = params.uid;
 	console.log(`getServerSideProps(${uid.join("/")})`);
 
-	const { folders, sharedOptions } = await APIClient.get(`/api/share/${uid[0]}`);
+	const { folders } = await APIClient.get(`/api/share/${uid[0]}`);
 
 	return {
 		props: {
 			folders,
-			sharedOptions,
 			path: uid.join("/")
 		}
 	};
 };
 
-export default withSharedFolderContext(CloudinaryFolder);
+/**
+ * We don't read the props. We rely on the loaded SharedFolderContext
+ */
+const SharedFolderPage = () => {
+	const {
+		folders,
+		current,
+		selectedTracks,
+		timestamp,
+		navigate
+	} = useSharedFolderContext();
+
+	// Load the current folder or display the selected tracks
+	const folder = folders[current] || selectedTracks;
+
+	if (!folder) return null;
+
+	const { path, subfolders, audios, videos, settings } = folder;
+
+	useEffect(() => {
+		console.log(`Exploring ${current}`, folder);
+	}, [current]);
+
+	return (
+		<Stack className="folder-content" minH="100vh" margin="0 1rem">
+			<SharedFolderNavigation path={path} settings={settings} />
+
+			{subfolders.length && (
+				<Grid
+					className="sub-folders"
+					templateColumns={{ sm: "repeat(2, 1fr)", lg: "repeat(6, 1fr)" }}
+					gap={5}
+					p={5}
+				>
+					{subfolders.map((path) => (
+						<Folder key={path} path={path} navigate={navigate} />
+					))}
+				</Grid>
+			)}
+
+			{audios.length && (
+				<AudioPlaylistPlayer
+					type="audio"
+					playerId="audio-player"
+					playlist={audios}
+					settings={settings}
+					timestamp={timestamp}
+				/>
+			)}
+
+			{videos.length && (
+				<VideoPlaylistPlayer
+					type="video"
+					playerId="video-player"
+					playlist={videos}
+					settings={settings}
+					timestamp={timestamp}
+				/>
+			)}
+		</Stack>
+	);
+};
+
+export default withSharedFolderContext(SharedFolderPage);

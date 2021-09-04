c3b180dac8a004058a5341af901fb4ce8c88f780
zipang <christophe.Desguez@gmail.com>
2021-09-04 20:14:05 +0200

    Share the new code to load the settings and playlist files

--- src/components/SharedSettings.js
+++ src/components/SharedSettings.js
@@ -2,16 +2,12 @@ import { Checkbox } from "@chakra-ui/checkbox";
 import { Stack } from "@chakra-ui/layout";
 import { useEffect, useState } from "react";
 import APIClient from "../lib/services/APIClient";
+import { SHARED_SETTINGS_DEFAULTS } from "../lib/services/CloudinaryClient";
 
 const SHARE_OPTIONS = {
 	download_links: "Téléchargement Individuel",
 	download_zip: "Tout Télécharger",
 	download_form: "Sélection + Formulaire"
-};
-const SHARE_OPTIONS_DEFAULTS = {
-	download_links: false,
-	download_zip: false,
-	download_form: false
 };
 
 /**
@@ -22,13 +18,13 @@ const SHARE_OPTIONS_DEFAULTS = {
 /**
  * Display the mini settings form for a shared folder
  */
-const SharedSettings = ({ folderPath, settings = SHARE_OPTIONS_DEFAULTS }) => {
-	const [sharedSettings, setSharedSettings] = useState(SHARE_OPTIONS_DEFAULTS);
+const SharedSettings = ({ folderPath, settings = SHARED_SETTINGS_DEFAULTS }) => {
+	const [sharedSettings, setSharedSettings] = useState(SHARED_SETTINGS_DEFAULTS);
 
 	const toggle = (code) => (evt) => {
 		sharedSettings[code] = !sharedSettings[code];
 
-		// Some options are in  fact exclusives from each other
+		// Some options are in fact exclusives from each other
 		if (sharedSettings[code] === true) {
 			switch (code) {
 				case "download_links":
@@ -40,7 +36,7 @@ const SharedSettings = ({ folderPath, settings = SHARE_OPTIONS_DEFAULTS }) => {
 			}
 		}
 		setSharedSettings({
-			...sharedSettings
+			...sharedSettings // create a new instance to re-render
 		});
 		console.log("Post new settings", sharedSettings);
 		APIClient.post("/api/settings/" + folderPath, { settings: sharedSettings });
@@ -56,7 +52,6 @@ const SharedSettings = ({ folderPath, settings = SHARE_OPTIONS_DEFAULTS }) => {
 			<h3>PARAMETRES</h3>
 			{Object.keys(SHARE_OPTIONS).map((code, i) => (
 				<Checkbox
-					title={folderPath}
 					name={code}
 					key={code}
 					value={code}

de556ac1e676c38d6e18a0c8a4ea569685b35628
zipang <christophe.Desguez@gmail.com>
2021-09-04 20:16:05 +0200

    Pass the share settings as a prop

--- src/components/player/AudioPlaylistPlayer.js
+++ src/components/player/AudioPlaylistPlayer.js
@@ -6,29 +6,30 @@ import Playlist from "./Playlist.js";
 const AudioPlayer = dynamic(() => import("./AudioPlayer.js"), { ssr: false });
 
 /**
- * @typedef VideoPlaylistPlayerProps
+ * @typedef AudioPlaylistPlayerProps
  * @property {String} playerId Unique id of the player where to send events
  * @property {Array} playlist list of tracks
+ * @property {Object} settings a map of settings keys
  */
 
 /**
  * Display a soundwave and a playlist of sounds
  * @param {AudioPlaylistPlayerProps} props
  */
-const AudioPlaylistPlayer = ({ playerId, playlist = [] }) => {
-	const {
-		sharedOptions: { addToSelection, directDownload, displayDownloadForm }
-	} = useSharedFolderContext();
+const AudioPlaylistPlayer = ({ playerId, playlist = [], settings = {} }) => {
+	// `download_form` allows to add the tracks to a playlist and download them later
+	// `download_links` allows to immediately download each tracks via a direct link
+	const { download_form = false, download_links = false } = settings;
 
 	// Build the list of columns to add to the DataTable
 	const columnsToDisplay = ["play"];
-	if (addToSelection) {
-		columnsToDisplay.push("artist", "song", "addToSelection");
+	if (download_form) {
+		columnsToDisplay.push("artist", "song", "download_form");
 	} else {
 		columnsToDisplay.push("title");
 	}
-	if (directDownload && !displayDownloadForm) {
-		columnsToDisplay.push("directDownload");
+	if (download_links) {
+		columnsToDisplay.push("download_links");
 	}
 	columnsToDisplay.push("duration");
 

f7df9d77e44a3af88610d52c6c00f4ac8da5c315
zipang <christophe.Desguez@gmail.com>
2021-09-08 23:25:58 +0200

    title is what is displayed. filename if the file name (used in the settings.playlist)

--- src/components/player/MiniPlaylist.js
+++ src/components/player/MiniPlaylist.js
@@ -6,7 +6,7 @@ import { useDialogContext } from "../base/Dialog";
 import { useEventBus } from "../EventBusProvider";
 import SvgBin from "../icons/SvgBin";
 
-const TrackEntry = ({ index, public_id, title, artist, filename, onDeleteAction }) => (
+const TrackEntry = ({ index, title, artist, onDeleteAction }) => (
 	<Grid
 		as="li"
 		templateColumns="4ch 1fr 1fr 3ch"
@@ -14,39 +14,43 @@ const TrackEntry = ({ index, public_id, title, artist, filename, onDeleteAction 
 		cursor="move"
 	>
 		<GridItem>#{index}</GridItem>
-		{title && <GridItem>{title}</GridItem>}
-		{artist && <GridItem>{artist}</GridItem>}
-		{filename && <GridItem colSpan={2}>{filename}</GridItem>}
+		{artist ? (
+			<>
+				<GridItem>{artist}</GridItem>
+				<GridItem>{title}</GridItem>
+			</>
+		) : (
+			<GridItem colSpan={2}>{title}</GridItem>
+		)}
 		<GridItem>
 			<SvgBin color="yellow" onClick={onDeleteAction} />
 		</GridItem>
 	</Grid>
 );
 
-const MiniPlaylist = ({ tracks = [], updatePlaylist }) => {
+const MiniPlaylist = ({ path, tracks = [], updatePlaylist }) => {
 	const { confirm } = useDialogContext();
 	const eb = useEventBus();
+	const [orderedTracks, setOrderedTracks] = useState(tracks);
 
 	useEffect(() => {
-		console.log("Re-rendering the tracks");
-	}, [tracks]);
+		console.log(`MiniPlaylist rendering the tracks for ${path}`, tracks);
+	}, [path, tracks]);
 
 	// Define the actions associated with each button
-	const deleteTrack = ({ filename, title, public_id }) => async (evt) => {
+	const deleteTrack = ({ title, public_id }) => async (evt) => {
 		const confirmDeletion = await confirm({
 			title: "TRACK SUPPRESSION",
-			message: `Supprimer '${title || filename}' du partage ?`
+			message: `Supprimer '${title}' du partage ?`
 		});
 		if (confirmDeletion) {
-			// Remove the resource  reloading the page
-			// setVTracks(vTracks.filter((track) => track.public_id !== public_id));
+			// Remove the resource
 			updatePlaylist(tracks.filter((track) => track.public_id !== public_id));
 			const { success } = await APIClient.del(`/api/resources/${public_id}`);
 			if (!success) {
 				alert(
-					`La suppression du partage '${
-						title || filename
-					}' n'a pu être effective. Il faut le supprimer totalement dans Cloudinary`
+					`La suppression du fichier partagé '${title}' n'a pu être effective. 
+Il faut le supprimer totalement dans Cloudinary`
 				);
 			}
 		}
@@ -54,13 +58,17 @@ const MiniPlaylist = ({ tracks = [], updatePlaylist }) => {
 	const playTrack = (track) => () => {
 		eb.emit("play:track", track);
 	};
+	const reorderTracks = (tracks) => {
+		updatePlaylist(tracks.map((track) => track.filename));
+		setOrderedTracks(tracks);
+	};
 
 	return (
 		<Box className="mini-playlist">
 			<h3>TRACKS</h3>
 			<Stack spacing={0}>
-				<ReactSortable tag="ul" list={tracks} setList={updatePlaylist}>
-					{tracks.map((track, i) => (
+				<ReactSortable tag="ul" list={orderedTracks} setList={reorderTracks}>
+					{orderedTracks.map((track, i) => (
 						<TrackEntry
 							key={track.public_id}
 							index={i + 1}

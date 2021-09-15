48c83d59cc9768321f490c38e582b61900010ce2
zipang <christophe.Desguez@gmail.com>
2021-09-15 18:55:16 +0200

    Renamed the link + select format

--- src/components/player/SelectDownloadFileFormat.js
+++ src/components/player/SelectDownloadFileFormat.js
@@ -5,22 +5,35 @@ import { useState } from "react";
 // @see https://cloudinary.com/documentation/audio_transformations#supported_audio_formats
 const _MUSIC_FORMAT_OPTIONS = ["wav", "mp3", "m4a", "ogg"];
 
-const SelectDownloadFormat = ({
+/**
+ * Generate a link+selectbox to directly download a file in the required format
+ * Or the whole playlist (multiple public_ids)
+ * @param {SelectDownloadFormat} props
+ */
+const SelectDownloadFileFormat = ({
 	file,
 	formatsOptions = _MUSIC_FORMAT_OPTIONS,
+	children,
 	...props
 }) => {
 	const [format, setFormat] = useState(formatsOptions[0]);
-	// Create the download URL (without the format)
-	const rawPath = "/download/" + file.url.split("upload/")[1].replace(/\.\w+$/i, "");
-	const niceFileName = file.artist
-		? `${file.artist} - ${file.title}.${format}`
-		: file.filename.replace(/\.\w+$/i, format);
+
+	// Create the download URL and proposed filename
+	let downloadUrl, downloadAs;
+
+	if (file) {
+		downloadUrl = "/download/" + file.url.split("upload/")[1].replace(/\.\w+$/i, "");
+		downloadAs = file.artist
+			? `${file.artist} - ${file.title}.${format}`
+			: file.filename.replace(/\.\w+$/i, format);
+	} else {
+		return null;
+	}
 
 	return (
 		<Box onClick={(evt) => evt.stopPropagation()} w="100%" h="100%">
-			<Link href={`${rawPath}.${format}`} download={niceFileName}>
-				Télécharger
+			<Link href={`${downloadUrl}.${format}`} download={downloadAs}>
+				{children || "Télécharger"}
 			</Link>
 			&nbsp;
 			<Select width="5rem" size="sm" display="inline-block" {...props}>
@@ -33,4 +46,4 @@ const SelectDownloadFormat = ({
 		</Box>
 	);
 };
-export default SelectDownloadFormat;
+export default SelectDownloadFileFormat;

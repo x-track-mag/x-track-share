9aef73a2de65dd8527fe1bf56695c4595f1cb7d1
zipang <christophe.Desguez@gmail.com>
2021-09-15 18:54:48 +0200

    Generate the download archive link with format selection

---
+++ src/components/player/SelectDownloadArchiveFormat.js
@@ -0,0 +1,40 @@
+import { Box } from "@chakra-ui/layout";
+import { Select } from "@chakra-ui/select";
+import { useState } from "react";
+
+/**
+ * Generate a link+selectbox to directly download a file in the required format
+ * Or the whole playlist (multiple public_ids)
+ * @param {SelectDownloadFormat} props
+ */
+const SelectDownloadArchiveFormat = ({
+	download_archive_links,
+	playlist_name = "playlist-" + new Date().toISOString().substr(0, 19),
+	children,
+	...props
+}) => {
+	const formatsOptions = Object.keys(download_archive_links) || [];
+	const [format, setFormat] = useState(formatsOptions[0] || "wav");
+	if (!download_archive_links) return null;
+
+	return (
+		<Box w="100%" h="100%" pt={4} textAlign="right">
+			<a
+				target="_blank"
+				href={download_archive_links[format]}
+				download={playlist_name}
+			>
+				{children || "Download Archive (zip)"}
+			</a>
+			&nbsp;
+			<Select width="5rem" size="sm" display="inline-block" {...props}>
+				{formatsOptions.map((format) => (
+					<option key={format} onClick={() => setFormat(format)}>
+						{format.toUpperCase()}
+					</option>
+				))}
+			</Select>
+		</Box>
+	);
+};
+export default SelectDownloadArchiveFormat;

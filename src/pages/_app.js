f7dfa75d92a6645f47552fe9a8b01d8bf226c3b7
zipang <christophe.Desguez@gmail.com>
2021-09-16 12:43:28 +0200

    Apply some global modifications on Promises, Strings and Arrays

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,19 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { DialogContextProvider } from "../components/base/Dialog.js";
 import EventBusProvider from "../components/EventBusProvider.js";
-
 import PageLayout from "../components/layout/PageLayout.js";
+import extendArrays from "../lib/utils/Arrays.js";
+import { exitOnRejections } from "../lib/utils/Promises.js";
+import extendStrings from "../lib/utils/Strings";
 import "../styles/x-track-theme.css";
 import theme from "../theme.js";
-import safePromises from "make-promises-safe";
 
 function MyApp({ Component, pageProps }) {
+	// Do some global tweakings
+	extendStrings();
+	extendArrays();
+	exitOnRejections();
+
 	return (
 		<ChakraProvider resetCSS theme={theme}>
 			<PageLayout>

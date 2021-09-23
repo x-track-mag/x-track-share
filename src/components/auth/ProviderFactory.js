0b9a9b9c0d9cb8129f7a02795bc4bb5d1272e381
zipang <christophe.Desguez@gmail.com>
2021-09-23 23:26:35 +0200

    Hard-coded value. (will try to figure how to pass it as environment parameter)

--- src/components/auth/ProviderFactory.js
+++ src/components/auth/ProviderFactory.js
@@ -29,15 +29,15 @@ export const getMicrosoftProvider = (params = {}) => {
 	const provider = new OAuthProvider("microsoft.com");
 	const { tenant } = params;
 
-	if (tenant) {
-		provider.setCustomParameters({
-			// Optional "tenant" parameter in case you are using an Azure AD tenant.
-			// eg. '8eaef023-2b34-4da1-9baa-8bc8c9d6a490' or 'contoso.onmicrosoft.com'
-			// or "common" for tenant-independent tokens.
-			// The default value is "common".
-			tenant
-		});
-	}
+	provider.setCustomParameters({
+		// Optional "tenant" parameter in case you are using an Azure AD tenant.
+		// eg. '8eaef023-2b34-4da1-9baa-8bc8c9d6a490' or 'contoso.onmicrosoft.com'
+		// or "common" for tenant-independent tokens.
+		// The default value is "common".
+		tenant: "x-track.net"
+	});
+	// if (tenant) {
+	// }
 
 	return provider;
 };

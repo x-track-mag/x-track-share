441daa92db7bba9e1be98e206050a12e7db240b3
zipang <christophe.Desguez@gmail.com>
2021-09-23 17:27:29 +0200

    Added primary prop

--- src/components/forms/inputs/Button.js
+++ src/components/forms/inputs/Button.js
@@ -1,7 +1,12 @@
 import { Button as ChakraUIButton } from "@chakra-ui/react";
 
-const Button = ({ children, ...props }) => (
-	<ChakraUIButton bgColor="blue" color="yellow" borderRadius={0} {...props}>
+const Button = ({ children, primary = true, ...props }) => (
+	<ChakraUIButton
+		bgColor={primary ? "blue" : "#333"}
+		color="yellow"
+		borderRadius={0}
+		{...props}
+	>
 		{children}
 	</ChakraUIButton>
 );

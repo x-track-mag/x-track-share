702662aa1e6c01b573789171b25b72b205f07fdf
zipang <christophe.Desguez@gmail.com>
2021-09-26 00:34:44 +0200

    Fix primary color bleeding

--- src/components/forms/inputs/Button.js
+++ src/components/forms/inputs/Button.js
@@ -2,7 +2,7 @@ import { Button as ChakraUIButton } from "@chakra-ui/react";
 
 const Button = ({ children, primary = true, ...props }) => (
 	<ChakraUIButton
-		bgColor={primary ? "blue" : "gray.500"}
+		bgColor={primary === true ? "blue" : "gray.500"}
 		color="yellow"
 		borderRadius={0}
 		{...props}

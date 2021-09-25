21f9593b9ffcc1f91a0466e0767bdb598f78a6d7
zipang <christophe.Desguez@gmail.com>
2021-09-25 23:39:47 +0200

    Defaukt style

--- src/components/forms/inputs/Button.js
+++ src/components/forms/inputs/Button.js
@@ -2,7 +2,7 @@ import { Button as ChakraUIButton } from "@chakra-ui/react";
 
 const Button = ({ children, primary = true, ...props }) => (
 	<ChakraUIButton
-		bgColor={primary ? "blue" : "#333"}
+		bgColor={primary ? "blue" : "gray.500"}
 		color="yellow"
 		borderRadius={0}
 		{...props}

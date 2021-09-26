c403ca2f2ca4c6e275666dcd1e36bc60e994fd17
zipang <christophe.Desguez@gmail.com>
2021-09-26 11:03:47 +0200

    FIX : Allow to pass refs to a functional component

--- src/components/forms/inputs/Button.js
+++ src/components/forms/inputs/Button.js
@@ -1,14 +1,15 @@
 import { Button as ChakraUIButton } from "@chakra-ui/react";
 
-const Button = ({ children, primary = true, ...props }) => (
+const Button = React.forwardRef(({ children, primary = true, ...props }, ref) => (
 	<ChakraUIButton
 		bgColor={primary === true ? "blue" : "gray.500"}
 		color="yellow"
 		borderRadius={0}
+		ref={ref}
 		{...props}
 	>
 		{children}
 	</ChakraUIButton>
-);
+));
 
 export default Button;

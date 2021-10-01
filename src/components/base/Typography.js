16831f509b9da8fe64cb3425ab75d11cc42a28d1
zipang <christophe.Desguez@gmail.com>
2021-10-01 07:35:04 +0200

    Adaptive tweaks

--- src/components/base/Typography.js
+++ src/components/base/Typography.js
@@ -1,40 +1,40 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 
 export const Title = ({ children, ...moreStyles }) => (
-	<Heading as="h2" fontSize="2rem" lineHeight="2.2rem" fontWeight={500} {...moreStyles}>
-		{children}
-	</Heading>
+    <Heading as="h2" fontSize="2rem" lineHeight="2.2rem" fontWeight={500} {...moreStyles}>
+        {children}
+    </Heading>
 );
 
 export const Subtitle = ({ children, ...moreStyles }) => (
-	<Heading as="h3" fontSize="1.6rem" lineHeight="2rem" fontWeight={500} {...moreStyles}>
-		{children}
-	</Heading>
+    <Heading as="h3" fontSize="1.7rem" lineHeight="2rem" fontWeight={500} {...moreStyles}>
+        {children}
+    </Heading>
 );
 
 export const ColumnHeader = ({ children, ...moreStyles }) => (
-	<Heading
-		as="h4"
-		fontSize="1.3rem"
-		lineHeight="1rem"
-		fontWeight={600}
-		color="white"
-		display="inline-block"
-		{...moreStyles}
-	>
-		{children}
-	</Heading>
+    <Heading
+        as="h4"
+        fontSize="1.4rem"
+        lineHeight="1rem"
+        fontWeight={600}
+        color="white"
+        display="inline-block"
+        {...moreStyles}
+    >
+        {children}
+    </Heading>
 );
 
 export const Info = ({ children }) => (
-	<Box as="p" className="info" lineHeight="1rem" fontSize="sm" color="white" mb="1rem">
-		{children}
-	</Box>
+    <Box as="p" className="info" lineHeight="1rem" fontSize="sm" color="white" mb="1rem">
+        {children}
+    </Box>
 );
 
 export default {
-	Title,
-	Subtitle,
-	ColumnHeader,
-	Info
+    Title,
+    Subtitle,
+    ColumnHeader,
+    Info
 };
--- src/styles/x-track-theme.js
+++ src/styles/x-track-theme.js
@@ -1,63 +1,64 @@
 import "@fontsource/archivo/800.css";
 
 export const fonts = {
-	heading: `'Conduit ITC W04 Regular', Archivo, sans`, // Conduit ITC W02 Bold
-	body: `'Conduit ITC W04 Regular', Raleway`
+    heading: `'Conduit ITC W04 Regular', Archivo, sans`, // Conduit ITC W02 Bold
+    body: `'Conduit ITC W04 Regular', Raleway`
 };
 
 export const breakpoints = {
-	sm: "480px",
-	md: "768px",
-	lg: "1080px",
-	xl: "1400px"
+    xs: "320px",
+    sm: "420px",
+    md: "768px",
+    lg: "1080px",
+    xl: "1400px"
 };
 
 export const colors = {
-	brand: {
-		black: "#000",
-		blue: "#00F",
-		yellow: "#FF0",
-		white: "#FFF"
-	}
+    brand: {
+        black: "#000",
+        blue: "#00F",
+        yellow: "#FF0",
+        white: "#FFF"
+    }
 };
 
 export const global = {
-	html: {
-		fontSize: {
-			sm: "18px",
-			md: "20px",
-			lg: "22px",
-			xl: "24px"
-		}
-	},
-	body: {
-		minWidth: "480px",
-		overflowX: "hidden",
-		color: "brand.yellow",
-		backgroundColor: "black",
-		"p, h1, h2, h3, h4": {
-			"&::selection": {
-				color: "brand.blue",
-				backgroundColor: "brand.yellow"
-			}
-		},
-		"h1, h2, h3, h4": {
-			textTransform: "uppercase",
-			fontWeight: 600
-		},
-		a: {
-			textDecoration: "underline"
-		}
-	}
+    html: {
+        fontSize: {
+            sm: "18px",
+            md: "20px",
+            lg: "22px",
+            xl: "24px"
+        }
+    },
+    body: {
+        minWidth: "480px",
+        overflowX: "hidden",
+        color: "brand.yellow",
+        backgroundColor: "black",
+        "p, h1, h2, h3, h4": {
+            "&::selection": {
+                color: "brand.blue",
+                backgroundColor: "brand.yellow"
+            }
+        },
+        "h1, h2, h3, h4": {
+            textTransform: "uppercase",
+            fontWeight: 600
+        },
+        a: {
+            textDecoration: "underline"
+        }
+    }
 };
 
 export const components = {
-	Progress: {
-		variants: {
-			"x-track": {
-				bg: "gray.700",
-				bgColor: "#ff0"
-			}
-		}
-	}
+    Progress: {
+        variants: {
+            "x-track": {
+                bg: "gray.700",
+                bgColor: "#ff0"
+            }
+        }
+    }
 };

b07997161ed98b2fca24476da075d84e7b4b11ac
zipang <christophe.Desguez@gmail.com>
2021-10-14 15:30:47 +0200

    Fix Warning: Invalid DOM property `fill-rule`. Did you mean `fillRule`?

--- src/components/icons/SvgTasks.js
+++ src/components/icons/SvgTasks.js
@@ -7,14 +7,14 @@ import Icon from "@chakra-ui/icon";
  */
 
 /**
- * Render a bin (trashcan) symbol
+ * Render a list of checked tasks
  * @param {SvgTasksProps} props
  */
 const SvgTasks = ({ color = "currentColor", size = "18px", ...props }) => (
 	<Icon height={size} width={size} viewBox="0 0 16 16" {...props}>
 		<path
 			fill={color}
-			fill-rule="evenodd"
+			fillRule="evenodd"
 			d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3.854 2.146a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 3.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 7.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"
 		/>
 	</Icon>

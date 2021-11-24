1b645781c70095ae02d90f1b8dacc6cdc488b7b6
zipang <christophe.Desguez@gmail.com>
2021-11-24 18:49:32 +0100

    Comment + get rid of unused navigation prop

--- src/components/layout/PageHeader.js
+++ src/components/layout/PageHeader.js
@@ -8,33 +8,33 @@ import SvgLogo from "../icons/SvgLogo";
  * @type PageHeaderProps
  * @property {String} title The textual reading of the logo
  * @property {CSSProp} height (Must be known to animate the hiding)
- * @property {Array} navigation
  */
 /**
- * Auto-hiding page header
+ * Auto-hiding Page Header
  * @param {PageHeaderProps} props
  */
-const PageHeader = ({ title = "", navigation = [], height = "3rem", ...props }) => {
-	const [visible, setVisible] = useState(true);
+const PageHeader = ({ title = "", height = "3rem", ...props }) => {
+	const [isVisible, setVisible] = useState(true);
 	const { scrollY, direction } = useVScrollPosition();
 
 	useEffect(() => {
+		// Disappear when we scroll more than 40 px and re-appear when we scroll up
 		setVisible(direction === "up" || scrollY < 40);
 	});
 
 	return (
 		<Box
-			as="header"
 			id="page-header"
+			as="header"
 			position="fixed"
 			height={height}
-			top={visible ? 0 : `-${height}`}
+			top={isVisible ? 0 : `-${height}`}
 			transition="top 0.5s ease-in-out"
 			zIndex="999"
 			right="0"
 			left="0"
-			{...props}
 			p="1rem"
+			{...props}
 		>
 			<Title id="header-logo">
 				<SvgLogo alt={title} title={title} color="yellow" />
